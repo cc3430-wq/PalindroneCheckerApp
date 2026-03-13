843c8c57e264aa7228c6299da671bc40f1c592fb
chandu33 <cc3430@srmist.edu.in>
2026-03-13 13:41:16 +0530

    chandu Added UC9-Recursive

--- App/src/PalindroneCheckerApp.java
+++ App/src/PalindroneCheckerApp.java
@@ -1,35 +1,24 @@
 import java.util.Scanner;
-import java.util.LinkedList;
-
 public class PalindroneCheckerApp {
+    private static boolean check(String s, int start, int end) {
+        if (start >= end)
+            return true;
 
-    public static boolean checkPalindrome(String input) {
-        LinkedList<Character> list = new LinkedList<>();
+        if (s.charAt(start) != s.charAt(end))
+            return false;
 
-        // Add characters to linked list
-        for (char c : input.toCharArray()) {
-            list.add(c);
-        }
-
-        // Compare first and last characters
-        while (list.size() > 1) {
-            if (!list.removeFirst().equals(list.removeLast())) {
-                return false;
-            }
-        }
-        return true;
+        return check(s, start + 1, end - 1);
     }
 
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Input: ");
         String input = sc.nextLine();
 
-        boolean result = checkPalindrome(input);
+        boolean result = check(input, 0, input.length() - 1);
 
         System.out.println("Is Palindrome?: " + result);
-
-        sc.close();
     }
 }
