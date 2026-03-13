7d4b5153adce2fc34bc8827656e6d125a6f948e6
chandu33 <cc3430@srmist.edu.in>
2026-03-13 13:36:29 +0530

    chandu Added UC8-LinkedListBased

--- App/src/PalindroneCheckerApp.java
+++ App/src/PalindroneCheckerApp.java
@@ -1,33 +1,35 @@
 import java.util.Scanner;
-import java.util.Stack;
-import java.util.Queue;
 import java.util.LinkedList;
-public class PalindroneCheckerApp{
-    public static void main(String[] args) {
 
-        System.out.println("Palindrome Checker App");
-        System.out.println();
+public class PalindroneCheckerApp {
 
-        Scanner sc = new Scanner(System.in);
+    public static boolean checkPalindrome(String input) {
+        LinkedList<Character> list = new LinkedList<>();
 
-        System.out.print("Input text: ");
-        String text = sc.nextLine();
-
-        Deque<Character> deque = new LinkedList<>();
-
-        for (char c : text.toCharArray()) {
-            deque.add(c);
+        // Add characters to linked list
+        for (char c : input.toCharArray()) {
+            list.add(c);
         }
 
-        boolean isPalindrome = true;
-
-        while (deque.size() > 1) {
-            if (!deque.removeFirst().equals(deque.removeLast())) {
-                isPalindrome = false;
-                break;
+        // Compare first and last characters
+        while (list.size() > 1) {
+            if (!list.removeFirst().equals(list.removeLast())) {
+                return false;
             }
         }
+        return true;
+    }
 
-        System.out.println("Is it a Palindrome? : " + isPalindrome);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Input: ");
+        String input = sc.nextLine();
+
+        boolean result = checkPalindrome(input);
+
+        System.out.println("Is Palindrome?: " + result);
+
+        sc.close();
     }
 }
